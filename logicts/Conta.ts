e8836de28d9c995c89e9f9bb6f433e32c3ce84c4
JoaoSousa2006 <130800970+JoaoSousa2006@users.noreply.github.com>
2026-09-08 19:50:34 -0300

    Refactor getters and setters in Conta class

--- logicts/Conta.ts
+++ logicts/Conta.ts
@@ -1,4 +1,5 @@
 export default abstract class Conta {
+    
     private _numbanco: number = 0.0;
     private _numconta: number = 0.0;
     private _saque: number = 0.0;
@@ -10,123 +11,62 @@ export default abstract class Conta {
     private _endereco: string ="";
     private _email: string ="";
 
-    //Getters and Setters
+    //Getters and Setter
 
-    public get numbanco(): number  {
-        return this._numbanco;
-    }
+        public get numbanco(): number {
+                return this._numbanco;
+        }
+        public set numbanco(value: number) {
+                this._numbanco = value;
+        }
+        public get numconta(): number {
+                return this._numconta;
+        }
+        public set numconta(value: number) {
+                this._numconta = value;
+        }
+        public get saque(): number {
+                return this._saque;
+        }
+        public set saque(value: number) {
+                this._saque = value;
+        }
+        public get deposito(): number {
+                return this._deposito;
+        }
+        public set deposito(value: number) {
+                this._deposito = value;
+        }
+        public get saldo(): number {
+                return this._saldo;
+        }
+        public set saldo(value: number) {
+                this._saldo = value;
+        }
 
-    
-    public get numconta(): number  {
-        return this._numconta;
-    }
-
-    
-    public get saque(): number  {
-        return this._saque;
-    }
-
-    
-    public get deposito(): number  {
-        return this._deposito;
-    }
-
-    
-    public set numbanco(value: number ) {
-        this._numbanco = value;
-    }
-
-    
-    public set numconta(value: number ) {
-        this._numconta = value;
-    }
-
-    
-    public set saque(value: number ) {
-        this._saque = value;
-    }
-
-    
-    public set deposito(value: number ) {
-        this._deposito = value;
-    }
-
-    // public set saldo(value: number ) {
-    //     this._saldo = value;
-    // }
-
-    public get saldos(): number {
-        return this._saldo;
-    }
-    public set saldos(value: number) {
-        this._saldo = value;
-    }
-
-
-    //Getters and Setters
-    
-    /**
-     * Getter nome
-     * @return {string}
-     */
-    public get nome(): string {
-        return this._nome;
-    }
-
-    /**
-     * Getter endereco
-     * @return {string}
-     */
-    public get endereco(): string {
-        return this._endereco;
-    }
-
-    /**
-     * Getter cpf
-     * @return {string}
-     */
-    public get cpf(): string {
-        return this._cpf;
-    }
-
-    /**
-     * Getter email
-     * @return {string}
-     */
-    public get email(): string {
-        return this._email;
-    }
-
-    
-    /**
-     * Setter nome
-     * @param {string} value
-     */
-    public set nome(value: string) {
-        this._nome = value;
-    }
-
-    /**
-     * Setter endereco
-     * @param {string} value
-     */
-    public set endereco(value: string) {
-        this._endereco = value;
-    }
-
-    /**
-     * Setter cpf
-     * @param {string} value
-     */
-    public set cpf(value: string) {
-        this._cpf = value;
-    }
-
-    /**
-     * Setter email
-     * @param {string} value
-     */
-    public set email(value: string) {
-        this._email = value;
-    }
+        //Cliente
+        public get nome(): string {
+                return this._nome;
+        }
+        public set nome(value: string) {
+                this._nome = value;
+        }
+        public get endereco(): string {
+                return this._endereco;
+        }
+        public set endereco(value: string) {
+                this._endereco = value;
+        }
+        public get cpf(): string {
+                return this._cpf;
+        }
+        public set cpf(value: string) {
+                this._cpf = value;
+        }
+        public get email(): string {
+                return this._email;
+        }
+        public set email(value: string) {
+                this._email = value;
+        }
 }
