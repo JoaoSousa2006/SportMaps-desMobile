34835934f45d20fd16a5e15c582b15a2618b7056
JoaoSousa2006 <130800970+JoaoSousa2006@users.noreply.github.com>
2026-09-08 19:53:40 -0300

    Refactor Corrente class methods and remove unused getters/setters
    Removed getters and setters for nome, endereco, cpf, and email. Updated depositar and sacar methods to modify saldo directly.

--- logicts/Corrente.ts
+++ logicts/Corrente.ts
@@ -9,87 +9,20 @@ export default class Corrente extends Conta implements IContas, IAbrir{
         this.endereco = cli._endereco;
         this.email = cli._email;
     }
+	
     depositar (valor: number): number {
-        this.deposito += valor;
+		
+        this.saldo += valor;
         return this.saldos();
     }
+	
     sacar (valor: number): number {
-        this.saque = valor;
-        return this.saldos() - this.saque;
+        this.saldo -= valor;
+        return valor;
     }
+	
     saldos (): number {
         return this.saldo;
     }
-
-
-    //Getters and Setters
-    
-    /**
-     * Getter nome
-     * @return {string}
-     */
-	public get nome(): string {
-		return this._nome;
-	}
-
-    /**
-     * Getter endereco
-     * @return {string}
-     */
-	public get endereco(): string {
-		return this._endereco;
-	}
-
-    /**
-     * Getter cpf
-     * @return {string}
-     */
-	public get cpf(): string {
-		return this._cpf;
-	}
-
-    /**
-     * Getter email
-     * @return {string}
-     */
-	public get email(): string {
-		return this._email;
-	}
-
-    private _nome: string;
-    /**
-     * Setter nome
-     * @param {string} value
-     */
-	public set nome(value: string) {
-		this._nome = value;
-	}
-
-    private _endereco: string;
-    /**
-     * Setter endereco
-     * @param {string} value
-     */
-	public set endereco(value: string) {
-		this._endereco = value;
-	}
-
-    private _cpf: string;
-    /**
-     * Setter cpf
-     * @param {string} value
-     */
-	public set cpf(value: string) {
-		this._cpf = value;
-	}
-
-    private _email: string;
-    /**
-     * Setter email
-     * @param {string} value
-     */
-	public set email(value: string) {
-		this._email = value;
-	}
 
 }
